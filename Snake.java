41f9cdede0ab724f49f0b31674395837f5130f9c
verrucktt <84430769+verrucktt@users.noreply.github.com>
2021-05-27 20:41:42 -0700

    Add files via upload
    Main function

---
+++ Snake.java
@@ -0,0 +1,20 @@
+/**
+ * @author	Emmanuel Bastidas
+ * @version 1.0
+ * This program is meant to learn the basics of JFrame and JPanel
+ * 
+ * A frame is a basic window of a program with title and border
+ * 
+ */
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Snake {
+
+	public static void main(String[] args) {
+		StartScreen myStartScreen = new StartScreen();
+	}
+
+}
